a849f5317b16380d1a71cb8d4f7284c09fd8c734
cstevens <colestevens24@gmail.com>
2023-03-29 11:26:12 -0700

    nicer rules

--- main.rb
+++ main.rb
@@ -167,14 +167,14 @@ end
 
 def rules 
     puts "----------------Rules!----------------
-    When started, the computer will create a 4 digit code.
-    The code will only contain the numbers 1-6.
-    Try to guess in as few moves as possible!
-    
-    After guessing, you will be shown how many digits you have in the correct position,
-    as well as how many correct digits you have in the incorrect position.
-    
-    Good Luck!    
+When started, the computer will create a 4 digit code.
+The code will only contain the numbers 1-6.
+Try to guess in as few moves as possible!
+
+After guessing, you will be shown how many digits you have in the correct position,
+as well as how many correct digits you have in the incorrect position.
+
+Good Luck!    
 --------------------------------------"
 end
 # def brokenCheck()
